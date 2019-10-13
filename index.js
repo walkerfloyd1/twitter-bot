99821bd4bb79e0e07d06d03bb4863496ccd611fb
Walker Floyd <walkerkeatley@yahoo.com>
2019-10-12 21:34:13 -0700

    more code

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const config = require('./config');
+
 const Twit = require('twit');
 
 const nickBot = new Twit(config);
 
+var users = ["517142263", "3215261003"];
+
+var stream = nickBot.stream('statuses/filter', {
+    follow: users
+});
+
+stream.on('tweet', function (tweet) {
+    if (users.indexOf(tweet.user.id_str) > -1) {
+        console.log(tweet.user.name + ": " + tweet.text);
+        nickBot.post('statuses/retweet/:id', {
+            id: tweet.id_str
+        }, function (err, data, response) {
+            console.log(data)
+        })
+    }
+});
