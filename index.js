79b1f82c07d0fc6f958cd9bd81da15d652422535
Walker Floyd <walkerkeatley@yahoo.com>
2019-10-12 21:36:58 -0700

    more code

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const config = require('./config');
 
 const Twit = require('twit');
 
-const nickBot = new Twit(config);
+const nickBot = new Twit({
+    consumer_key: "eBkrxtzKU2FYqXCYJtUzr8gtg",
+    consumer_secret: "6utlBvPvueFeanPWVHK4RFYbq375QwkY07FWMgUZzTP3OJcnOv",
+    access_token: "517142263-FP8HrJrf3lEGIjtgY2x4zHnKTo2Y6sp8AEMgcQrd",
+    access_token_secret: "qA0QZUOW4xccrrQNN7NZ5YhYcvI7uyWNS8UZy7LfW58nd"
+});
 
 var users = ["517142263", "3215261003"];
 
