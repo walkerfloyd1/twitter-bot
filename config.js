b54ff5219de9dc2af206557aa865e97157cafd17
Walker Floyd <walkerkeatley@yahoo.com>
2019-10-12 21:38:01 -0700

    removed files

--- config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-module.exports = ({
-    consumer_key: process.env.BOT_CONSUMER_KEY,
-    consumer_secret: process.env.BOT_CONSUMER_SECRET,
-    access_token: process.env.BOT_ACCESS_TOKEN,
-    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
-})
